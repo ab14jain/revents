312c3dd45518c1789162d28ef18a050f1d669b5a
Abhishek kumar jain <abhishekkumar.jain@outlook.com>
2019-11-06 20:36:30 +0530

    updated firestore config

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -4,16 +4,7 @@ import 'firebase/database';
 import 'firebase/auth';
 import 'firebase/storage';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyA4XkhfCrR0ENSjici6UIlh3EtRdZlafpg",
-    authDomain: "revents-257708.firebaseapp.com",
-    databaseURL: "https://revents-257708.firebaseio.com",
-    projectId: "revents-257708",
-    storageBucket: "revents-257708.appspot.com",
-    messagingSenderId: "247034762970",
-    appId: "1:247034762970:web:1cb61e415cc6f618f5b7e3",
-    measurementId: "G-Q9VHXYYD1Q"
-};
+
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
